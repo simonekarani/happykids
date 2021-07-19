183b3f97dff7295cd66919808fdee178bb9e4114
Simone Karani <simone.karani@gmail.com>
2021-07-19 00:12:20 -0700

    smile first view

--- happykids/Program/SmileFirstViewController.swift
+++ happykids/Program/SmileFirstViewController.swift
@@ -9,13 +9,51 @@ import UIKit
 
 class SmileFirstViewController: UIViewController {
 
+    @IBOutlet weak var emailSmileLabel: UILabel!
+    @IBOutlet weak var callSmileLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        emailSmileLabel.isUserInteractionEnabled = true
+        callSmileLabel.isUserInteractionEnabled = true
+        setupLabelInteractions()
     }
     
 
+    func setupLabelInteractions() {
+        let gesture1 = UITapGestureRecognizer(target: self, action: #selector(emailSmileClicked(_:)))
+        gesture1.numberOfTapsRequired = 1
+        emailSmileLabel.addGestureRecognizer(gesture1)
+
+        let gesture2 = UITapGestureRecognizer(target: self, action: #selector(callSmileClicked(_:)))
+        gesture2.numberOfTapsRequired = 1
+        callSmileLabel.addGestureRecognizer(gesture2)
+    }
+    
+    @objc func emailSmileClicked(_ sender: Any) {
+        /*if #available(iOS 10.0, *) {
+            guard let url = URL(string: "tel://14085645114"),
+                UIApplication.shared.canOpenURL(url) else {
+                    return
+            }
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        } else {
+            
+        }*/
+    }
+
+    @objc func callSmileClicked(_ sender: Any) {
+        if #available(iOS 10.0, *) {
+            guard let url = URL(string: "tel://14085645114"),
+                UIApplication.shared.canOpenURL(url) else {
+                    return
+            }
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        } else {
+            
+        }
+    }
     /*
     // MARK: - Navigation
 
